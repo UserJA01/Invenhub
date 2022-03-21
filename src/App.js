8a4d46361e7ee3ec7a1812ddc39ed5929b6bce1d
Milton <A01634505@itesm.mx>
2022-03-21 12:44:26 -0600

    Added inventory interface

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import Login from "./Views/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./Views/Dashboard";
+import Entry from "./Views/Entry";
+import Inventory from "./Views/Inventory";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dash" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Login />} />
+                    <Route path='/dash' element={<Dashboard />} />
+                    <Route path='/entry' element={<Entry />} />
+                    <Route path='/inventory' element={<Inventory />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
